5476840f50238f5ba7c725ac9aa94cdbe85a6572
nicolenahh <89764765+nicolenahh@users.noreply.github.com>
2024-07-21 19:57:01 +0800

    Update index.jsx
    change button title

--- app/index.jsx
+++ app/index.jsx
@@ -36,7 +36,7 @@ export default function App() {
           </Text>
 
           <CustomButton 
-          title="Continue with Email"
+          title="Sign in"
           handlePress={() => router.push('/sign-in')}
           containerStyles="w-full mt-7"
           />
