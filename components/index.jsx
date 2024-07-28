d86fb65eed2aba8c294d234d98b64fff5d481b7a
Nicole <nicolenajq@yahoo.com.sg>
2024-07-28 22:23:28 +0800

    index.jsx

---
+++ components/index.jsx
@@ -0,0 +1,13 @@
+import FormField from "./FormField";
+import CustomButton from "./CustomButton";
+import InfoBox from "./InfoBox";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
+
+export {
+  FormField,
+  CustomButton,
+  InfoBox,
+  SearchInput,
+  EmptyState,
+};
